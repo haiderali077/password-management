61b77d0f9def5fcbf9c011d25af8b025e0386f59
haiderali077 <haiderali077@users.noreply.github.com>
2024-08-20 22:48:08 +0500

    fucntions added: send passwords to frontend, encrypt passwords and store to database, decrypt passwords and pass to frontend

--- backend/backend.js
+++ backend/backend.js
@@ -3,9 +3,7 @@ const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
 const  PORT = 3001;
-
-
-
+const {encrypt, decrypt} = require('./encryption.js');
 
 
 app.use(cors());
@@ -20,13 +18,13 @@ const db = mysql.createConnection({
 
 
 app.post("/addpassword", (req, res) => {
-    const {password, title} = req.body; 
+    const {password, title} = req.body;   
 
-  
+    const encryptedPassword = encrypt(password);
 
     db.query(
-        "INSERT INTO passwords (password, title, iv) VALUES (?,?)", [
-        encryptedPassword.password, title],
+        "INSERT INTO passwords (password, title, iv) VALUES (?,?, ?)", [
+        encryptedPassword.password, title, encryptedPassword.iv],
         (err, result) => {
              if(err) {
                 console.log(err);
@@ -38,6 +36,26 @@ app.post("/addpassword", (req, res) => {
     );
 
 });
+
+app.get("/getPasswords", (req, res) => {
+
+    db.query(
+        "SELECT * FROM passwords", (err, results) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(results);
+            }
+        }
+    )
+
+}); 
+
+app.post("/decryptedpassword", (req, res) => {
+    res.send(decrypt(req.body))
+})
+
+
 
 
 app.get('/', (req, res) => {
