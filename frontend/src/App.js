993981e24910c6eb215115101711e4bc30da2cc4
haiderali077 <haiderali077@users.noreply.github.com>
2024-08-21 10:00:02 +0500

    password display button added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 
-import './defaultStyle.css';
+import './App.css';
 import { useState, useEffect } from 'react';
 import Axios from 'axios';
+
+
 
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
 
   const [password, setPassword] = useState("");
   const [title, setTitle] = useState("");
+  const [passwordList, setPasswordList] = useState([]);
  
 
   const addPassword = (event) => {
@@ -21,13 +24,42 @@ function App() {
 
   }
 
+  useEffect(() => {
+    Axios.get('http://localhost:3001/getPasswords').then((response) => {
+      setPasswordList(response.data);
+    });
+  }, [])
+
 
   const apiRequest = () => {
     Axios.post('http://localhost:3001/addpassword', {
       password: password,
-    title: title,
+     title: title,
     }) ;
   }
+
+
+  const decrypt = (encrypt) => {
+    Axios.post('http://localhost:3001/decryptedpassword', {
+      password: encrypt.password,
+      id: encrypt.id,
+      iv: encrypt.iv,
+     }
+    ).then((response) => {
+      setPasswordList(passwordList.map((value) => {
+        return value.id === encrypt.id ? 
+        {
+          id: value.id,
+          password: value.password,
+          title: response.data,
+          iv: value.iv,
+        } : value
+      }))
+    })
+  }
+
+
+  
 
   
 
@@ -47,9 +79,28 @@ function App() {
           <button onClick={apiRequest}>Add Password</button>
       </div>
 
-      
-       
+      <div className="showPasswords">
+        { passwordList.map((value, key) => {
+          return (
+          <div className="password" onClick={() => {decrypt(
+            {
+              password: value.password,
+              iv: value.iv,
+              id: value.id,
+            }
+          )
+        }}  
+          key = {key}
+          >
+            <h3>{value.title}</h3>
+
+          </div>
+          )
+        })}
+
       </div>
+
+    </div>
       
   
   );
